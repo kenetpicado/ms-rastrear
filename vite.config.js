5a1a57e5b962f593168f13a51ae1d78f890ae45e
Kenet <kenetpicado1@gmail.com>
2024-12-08 12:00:53 -0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ms-rastrear',
-  //base: '/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
